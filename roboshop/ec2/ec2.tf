3149c6a2c1752413021a369a9593c7caa67f24dc
venkatapradeep36 <venkatapradeepgutta2@gmail.com>
2021-07-13 17:43:17 +0530

    creating multiple instances

--- roboshop/ec2/ec2.tf
+++ roboshop/ec2/ec2.tf
@@ -1,10 +1,11 @@
 resource "aws_spot_instance_request" "cheap_worker" {
-  ami           = "ami-059e6ca6474628ef0"
-  spot_price    = "0.0031"
-  instance_type = "t3.micro"
+  count                   = length(var.COMPONENTS)
+  ami                     = "ami-059e6ca6474628ef0"
+  spot_price              = "0.0031"
+  instance_type           = "t3.micro"
 
   tags = {
-    Name = "sample"
+    Name                  = "sample"
   }
 }
 
